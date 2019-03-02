401c0b09b21ebbd07527ec0e95d81a359b1db2d3
Sha <zhenysha@amazon.com>
2019-03-01 16:39:47 -0800

    change module name

--- tutorials/nnvm/deploy_mxnet_on_remote_gpu.py
+++ tutorials/nnvm/deploy_mxnet_on_remote_gpu.py
@@ -152,8 +152,7 @@ if local_demo:
     target = tvm.target.create('llvm')
 else:
     target = tvm.target.create('cuda')
-    # The above line is a simple form of
-    # target = tvm.target.create('llvm -device=arm_cpu -model=bcm2837 -target=armv7l-linux-gnueabihf -mattr=+neon')
+    
 
 with nnvm.compiler.build_config(opt_level=3):
     graph, lib, params = nnvm.compiler.build(
@@ -165,7 +164,7 @@ with nnvm.compiler.build_config(opt_level=3):
 
 # Save the library at local temporary directory.
 tmp = util.tempdir()
-lib_fname = tmp.relpath('net.tar')
+lib_fname = tmp.relpath('mxnet.tar')
 lib.export_library(lib_fname)
 
 ######################################################################
@@ -185,7 +184,7 @@ else:
 
 # upload the library to remote device and load it
 remote.upload(lib_fname)
-rlib = remote.load_module('net.tar')
+rlib = remote.load_module('mxnet.tar')
 
 # create the remote runtime module
 ctx = remote.gpu(0)
